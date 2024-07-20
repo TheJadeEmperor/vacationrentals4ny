be7348ed9a17c4fa30409228854af57a21e6f429
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-07-20 00:25:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,36 +5,119 @@ $adminEmail = 'kaiba.corporation.llc@gmail.com';
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>KaibaCorp Property Rentals & Tours</title>
+		<title>KaibaCorp Vacation Rentals & Tours</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
+<style> 
 
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+}
+
+.brand-title {
+    font-size: 1.5em;
+    color: white;
+}
+
+.navbar-links {
+    height: 100%;
+}
+
+.navbar-links ul {
+    display: flex;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+
+.navbar-links li {
+    margin-left: 0px;
+}
+
+.navbar-links a {
+    color: white;
+    text-decoration: none;
+    font-size: 1em;
+}
+
+.navbar-links a:hover {
+    color: #ff6347;
+}
+
+.toggle-button {
+    display: none;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 30px;
+    height: 21px;
+}
+
+.toggle-button .bar {
+    height: 3px;
+    width: 100%;
+    background-color: white;
+    border-radius: 10px;
+}
+
+@media (max-width: 900px) {
+    .navbar-links {
+        display: none;
+        width: 100%;
+    }
+
+    .navbar-links ul {
+        flex-direction: column;
+        width: 100%;
+    }
+
+    .navbar-links ul li {
+        text-align: center;
+        margin: 10px 0;
+    }
+}
+
+.navbar.active .navbar-links {
+    display: flex;
+}
+
+</style>
 		<!-- Wrapper -->
 			<div id="wrapper">
 
 				<!-- Header -->
 					<header id="header" class="alt">
-						<a href="index.html" class="logo"><strong>South Brooklyn</strong> <span>Airbnb</span></a>
+						<a href="index.html" class="logo"><strong>KaibaCOrp</strong> <span>Vacation Rentals</span></a>
 
-						<p>...</p>
+						<nav class="navbar">
+						 
+							 
+							<div class="navbar-links">
+								<ul>
+									<li><a href="#home">Home</a></li>
+									<li><a href="#gallery">Gallery</a></li>
+									<li><a href="#tours">Tours</a></li>
+									<li><a href="#contact">Contact</a></li>
+								</ul>
+							</div>
+						</nav>
 						
 						<nav>
 							<a href="#menu">Menu</a>
 						</nav>
 					</header>
 
+
+
 				<!-- Menu -->
 					<nav id="menu">
-						<!--<ul class="links">
-							<li><a href="#">Home</a></li>
-							<li><a href="#gallery">Gallery</a></li>
-							<li><a href="#for_sale">For Sale</a></li> 
-						</ul>-->
-						
+ 
 						<ul class="actions stacked">
 
 							<li><a href="#" class="button primary fit scrolly">Home</a></li>
@@ -321,11 +404,45 @@ Enhance your accessory collection with this classic Gucci belt. Its timeless des
 					</div>
 				</section>
 
+				<?php
+
+$adminEmail = 'kaiba.corporation.llc@gmail.com';
+
+	if($_POST['message']) {
+	$error = preg_match('/\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i', $_POST['email']) ? '' : 'INVALID EMAIL ADDRESS';
+
+	$message = $_POST['message'];
+	$name = $_POST['name']; 
+	$email = $_POST['email'];
+		
+	if(!$error) {
+		$headers = "From: ".$adminEmail."\n"; 
+
+		$emailSubject = "KaibaCorp: Your Message Was Received";
+		$emailContent = "You have sent a message to BL Web Solutions. The contents
+of the message are the following:
+		
+Full Name: ".$name."
+Email: ".$email."
+Message: ".$message."";
+		
+		if(@mail($email.','.$adminEmail, $emailSubject, $emailContent, $headers)) { 
+			$error = 'Message sent! You will receive a confirmation email shortly.'; 
+		} 
+		else {
+			$error = 'Error: message not sent! Please inform the administrator: '.$adminEmail;
+		}
+	}
+}
+
+?>
+
 				<!-- Contact -->
 					<section id="contact">
 						<div class="inner">
 							<section>
-								<form method="post" action="#">
+								<form method="post" action="./#contact">
+									<div><?=$error?></div>
 									<div class="fields">
 										<div class="field half">
 											<label for="name">Name</label>
