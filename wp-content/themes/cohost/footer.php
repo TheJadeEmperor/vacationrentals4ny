1bc09edebc1d21228dab0e3257f4714ccd91b2ef
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-09-13 23:33:06 -0400

    Update footer.php

--- wp-content/themes/cohost/footer.php
+++ wp-content/themes/cohost/footer.php
@@ -9,7 +9,7 @@
 
 <?php
 
-	if($_POST['message']) {
+	if($_POST['email']) {
 	$error = preg_match('/\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i', $_POST['email']) ? '' : 'INVALID EMAIL ADDRESS';
 
 	$message = $_POST['message'];
