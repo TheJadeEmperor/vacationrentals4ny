db4a724cfd89ce37c4afbb6d860f2f277b2db498
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-09-14 11:59:19 -0400

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ phpinfo();
 
 ini_set( 'display_errors', 1 );
 error_reporting( E_ALL );
-$from = "mymail@myawesomedomain.tld";
+$from = "kaiba.corporation.llc@gmail.com";
 $to = "kaiba.corporation.llc@gmail.com";
 $subject = "Checking PHP mail";
 $message = "PHP mail works just fine";
